e5ee812b66331c83986f4b3e1903fdeb9af1719a
ronnym <ronny@terasky.com>
2022-05-29 18:32:59 +0300

    commit

--- 10.2.py
+++ 10.2.py
@@ -8,3 +8,5 @@ def validate(prompt, low, high, ok, not_ok):
 
 validate("enter your age: ", 0, 120, "age is good", "age is bad")
 validate("enter amount of pets: ", 0, 4, "you are a good person", "you are a better person")
+
+print("f")
